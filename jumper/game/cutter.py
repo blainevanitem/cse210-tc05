4305286cd89e6e73e0ebe6659ef9068a391c99f1
Steven Briskey <stevenjb96@gmail.com>
2021-05-19 10:57:37 -0600

    skeleton

---
+++ jumper/game/cutter.py
@@ -0,0 +1,6 @@
+class Cutter:
+    def cut_parachute(self):
+        '''
+        for loop to see if letter in word.
+        return true or false
+        '''
---
+++ jumper/game/jumper.py
@@ -0,0 +1,8 @@
+class Jumper:
+    def get_guess(self):
+        #call display guess_prompt
+        pass
+    def is_letter(self):
+        '''
+        compare letter to word
+        '''
