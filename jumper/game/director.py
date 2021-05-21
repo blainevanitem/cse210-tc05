757012247e27bddc32a72725f8945392ec3ff2f3
Blaine <van17021@byui.edu>
2021-05-21 15:27:39 -0600

    changes to director

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -14,8 +14,22 @@ class Director:
         self.cutter = Cutter()
 
     def start_game(self):
+        #need to select the word first and get spaces.
         while(self.keep_playing):
-            self.console.display_spaces()
-            self.console.display_parachuter()
-            self.jumper
-            self.cutter
+            self.do_outputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def do_outputs(self):
+        self.console.display_spaces()
+        self.console.display_parachuter()
+
+    def do_updates(self):
+        self.cutter.cut_parachute()
+        #need to update the blank word array with correct letter
+
+    def get_inputs(self):
+        self.jumper.get_guess()
+
+
+    
