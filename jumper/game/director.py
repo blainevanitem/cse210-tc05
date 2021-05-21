e79d0cdf8e1ad8d14f27592da1edb01a5db304fe
Blaine <van17021@byui.edu>
2021-05-21 15:09:57 -0600

    changes

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -7,7 +7,7 @@ class Director:
     def __init__(self):
         self.keep_playing = True
         self.word_list = ["Example"]
-        self.parachuter = ["  ___  "],[" /___\ "],[" \   / "],["  \ /  "],["   0   "],["  /|\  "],["  / \  "],["       "],["^^^^^^^"]
+        self.parachuter = ["  ___  "," /___\ "," \   / ","  \ /  ","   0   ","  /|\  ","  / \  ","       ","^^^^^^^"]
         self.cut_parachute = False
         self.console = Console()
         self.jumper = Jumper()
@@ -15,6 +15,7 @@ class Director:
 
     def start_game(self):
         while(self.keep_playing):
-            self.console
+            self.console.display_spaces()
+            self.console.display_parachuter()
             self.jumper
             self.cutter
