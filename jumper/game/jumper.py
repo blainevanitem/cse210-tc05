245e6456835771c699eeb75e36747f6cfd0401b6
Steven Briskey <stevenjb96@gmail.com>
2021-05-21 15:12:35 -0600

    more jumping

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,15 +1,22 @@
+import game.console
+
 class Jumper:
     def __init__(self):
         empty_word = []
+        guess = ""
+        console = Console()
+        
 
-    def empty_word(self,word):
+    def blank_word(self,word):
+        #Used to create a list with _ for each letter in word
         for i in range(0,len(word)):
+            #For each letter in the word add _ to empty list
             self.empty_word.append("_")
 
     def get_guess(self):
         #call display guess_prompt
         message = "What letter do you think is in my word"
-        return message
+        self.guess = self.console.display_message(message)
         
     def is_letter(self, guess, word):
         '''
@@ -23,6 +30,3 @@ class Jumper:
             for _ in range(1, times):
                 space = word.index(guess,space)
                 self.empty_word.insert(guess, space)
-
-
-
