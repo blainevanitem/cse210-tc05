19bd619d74a993bfcb64ec606d1a8bdd20c3aa4a
darkauradragon <dragonfireangel@live.com>
2021-05-21 16:53:12 -0600

    we did the things

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,12 +1,21 @@
-from game.console import Console
 
 class Jumper:
     def __init__(self):
-        self.guess = ""
-        self.console = Console()
+        self.guess = ""        
+
         
-    def get_guess(self):
-        #call display guess_prompt
-        message = "What letter do you think is in my word: "
-        self.guess = self.console.display_message(message)
+    def is_letter(self,letter, word):
+        '''
+        compare letter to word
+        '''
+        self.guess = letter
+        times = word.count(self.guess)
         
+        
+        for _ in range(0, times):
+            space = word.index(self.guess,space)
+            self.empty_word.insert(self.guess, space)
+        if times == 0:
+            return False
+        else:
+            return True
