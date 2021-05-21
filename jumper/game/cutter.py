8351031e6040dc53cd1cc2efd3d1ef89c2e8a4c3
darkauradragon <dragonfireangel@live.com>
2021-05-21 14:34:13 -0600

    CHeck mi code

--- jumper/game/cutter.py
+++ jumper/game/cutter.py
@@ -1,6 +1,16 @@
+get_word = ""
 class Cutter:
-    def cut_parachute(self):
+    def get_word(self, word_list):
+        # get word, save word to self.word
+        self.word_list = get_word
+    def cut_parachute(self, get_guess):
+        
         '''
         for loop to see if letter in word.
         return true or false
         '''
+        for self.get_guess in get_word:
+            if self.get_guess == True:
+                return False
+            else:
+                return True
